665d098fd14ed8137480713825a11bb9394c10fa
hdfeng265 <sleepingmad@gmail.com>
2017-04-14 16:06:31 +0800

    initial

---
+++ src/main/java/com/hdfeng/JenkinsHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.hdfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHelloworldApplication.class, args);
+	}
+}
